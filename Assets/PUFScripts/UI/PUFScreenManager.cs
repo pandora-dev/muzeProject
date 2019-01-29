63f5d80c90666c805464f5f91201002d3468c743
Kemal Akcali <kemal@pandorareality.com>
2019-01-29 22:44:25 +0300

    PUFScreenManager class added
    All UI Screen Management will be handled here

---
+++ Assets/PUFScripts/UI/PUFScreenManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PUFScreenManager : MonoBehaviour
+{
+    void Awake()
+    {
+        if(this.GetComponent<Canvas>()==null){
+            Debug.LogError("This script must be attached into Canvas");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
