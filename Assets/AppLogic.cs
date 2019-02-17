4eb397e108505b13df3a0225b5b8fb2c780394bb
Kemal Akcali <kemal@pandorareality.com>
2019-02-18 00:59:48 +0300

    ARButton çalışır hale getirildi
    ARButton fonksiyonsuz olarak çalışır hale geldi (Animasyonlar vb.)

--- Assets/AppLogic.cs
+++ Assets/AppLogic.cs
@@ -13,13 +13,15 @@ public class AppLogic : MonoBehaviour
     public HorizontalScrollSnap tutorialHorizontalScrollSnap;
     public PaginationManager tutorialPaginationManager;
     public ButtonOneClickable ARButton;
-    public ARButtonState arButtonState; 
-    public enum ARButtonState { OPEN,CLOSE};
+    public ARButtonState arButtonState;
+    public bool emulateWithMouse;
 
+
+    public enum ARButtonState { OPEN,CLOSE};
     GraphicRaycaster m_Raycaster;
     EventSystem m_EventSystem;
     PointerEventData m_PointerEventData;
-
+    
 
     public void Start()
     {
@@ -62,33 +64,86 @@ public class AppLogic : MonoBehaviour
 
     public void Update()
     {
-        /*
-            m_PointerEventData = new PointerEventData(m_EventSystem);
-            m_PointerEventData.position = Input.mousePosition;
-            List<RaycastResult> results = new List<RaycastResult>();
-            m_Raycaster.Raycast(m_PointerEventData, results);
 
-            foreach (RaycastResult result in results)
+
+        m_PointerEventData = new PointerEventData(m_EventSystem);
+        if (emulateWithMouse)
+        {
+            if(Input.GetMouseButtonDown(0))
             {
-                if(result.gameObject.name)
-                Debug.Log("Hit " + result.gameObject.name);
+                m_PointerEventData.position = Input.mousePosition;
+                if (arButtonState == ARButtonState.OPEN)
+                {
+                    if (IsInsideClickForARButton())
+                        RunAR();
+                    else
+                        ARButtonDown();
+                }
             }
+
         }
-        */
+        else if(Input.touches.Length > 0)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    m_PointerEventData.position = touch.position;
+                    if(arButtonState==ARButtonState.OPEN)
+                    {
+                        if (IsInsideClickForARButton())
+                            RunAR();
+                        else
+                            ARButtonDown();
+                    }
+                }
+            }
+                    
+        }
+
+        
+            
+        
+        
+        
+        
     }
+
+    private bool IsInsideClickForARButton()
+    {
+        bool ARClickFound = false;
+        List<RaycastResult> results = new List<RaycastResult>();
+        m_Raycaster.Raycast(m_PointerEventData, results);
+
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject.name == "ARButton")
+                ARClickFound = true;
+        }
+        return ARClickFound;
+    }
+
+    public void RunAR()
+    {
+        Debug.Log("AR is Running");
+    }
+
 
     public void ARButtonUp()
     {
-        ARButton.GetComponent<Animator>().SetTrigger("ARTriggered");
+        ARButton.GetComponent<Animator>().SetBool("ARCancel", false);
+        ARButton.GetComponent<Animator>().SetBool("ARTrigger",true);
         arButtonState = ARButtonState.OPEN;
     }
     public void ARButtonDown()
     {
-        pScreenManager.views[1].GetComponent<Animator>().SetTrigger("ARTriggered");
+        ARButton.GetComponent<Animator>().SetBool("ARTrigger", false);
+        ARButton.GetComponent<Animator>().SetBool("ARCancel",true);
         arButtonState = ARButtonState.CLOSE;
     }
 
 
+    
     private bool tutorialPageLatestSwipe()
     {
         return (isTutorialPage() && tutorialHorizontalScrollSnap.CurrentPage == tutorialHorizontalScrollStepCount - 1);
